e4ad67e0aeff5aa6c74d0b0853019a30e3834b85
Osvaldo González <47746727+OsvaldoGonzalezPy@users.noreply.github.com>
2023-11-24 09:46:51 -0300

    Add files via upload

--- Plugin/region_type_plugin_og_google_maps_search.sql
+++ Plugin/region_type_plugin_og_google_maps_search.sql
@@ -3,59 +3,55 @@ set define off verify off feedback off
 whenever sqlerror exit sql.sqlcode rollback
 --------------------------------------------------------------------------------
 --
--- Oracle APEX export file
+-- ORACLE Application Express (APEX) export file
 --
--- You should run this script using a SQL client connected to the database as
--- the owner (parsing schema) of the application or as a database user with the
--- APEX_ADMINISTRATOR_ROLE role.
---
--- This export file has been automatically generated. Modifying this file is not
--- supported by Oracle and can lead to unexpected application and/or instance
--- behavior now or in the future.
+-- You should run the script connected to SQL*Plus as the Oracle user
+-- APEX_200200 or as the owner (parsing schema) of the application.
 --
 -- NOTE: Calls to apex_application_install override the defaults below.
 --
 --------------------------------------------------------------------------------
 begin
-wwv_flow_imp.import_begin (
- p_version_yyyy_mm_dd=>'2023.10.31'
-,p_release=>'23.2.0'
-,p_default_workspace_id=>51803836654824927748
-,p_default_application_id=>239526
+wwv_flow_api.import_begin (
+ p_version_yyyy_mm_dd=>'2020.10.01'
+,p_release=>'20.2.0.00.20'
+,p_default_workspace_id=>100001
+,p_default_application_id=>100
 ,p_default_id_offset=>0
-,p_default_owner=>'WKSP_WSOG'
+,p_default_owner=>'TEST'
 );
 end;
 /
  
-prompt APPLICATION 239526 - APEX Lab
+prompt APPLICATION 100 - APEX Lab
 --
 -- Application Export:
---   Application:     239526
---   Name:            APEX Lab
---   Date and Time:   22:17 Thursday November 23, 2023
---   Exported By:     OVALGONZALEZ@GMAIL.COM
+--   Application:     100
+--   Name:            APEX LAb
+--   Date and Time:   10:43 Friday November 24, 2023
+--   Exported By:     OSVALDOGONZALEZ
 --   Flashback:       0
 --   Export Type:     Component Export
 --   Manifest
---     PLUGIN: 21888107920039365562
+--     PLUGIN: 270922179615924351
 --   Manifest End
---   Version:         23.2.0
---   Instance ID:     63113759365424
+--   Version:         20.2.0.00.20
+--   Instance ID:     300168001583092
 --
 
 begin
   -- replace components
-  wwv_flow_imp.g_mode := 'REPLACE';
+  wwv_flow_api.g_mode := 'REPLACE';
 end;
 /
 prompt --application/shared_components/plugins/region_type/og_google_maps_search
 begin
-wwv_flow_imp_shared.create_plugin(
- p_id=>wwv_flow_imp.id(21888107920039365562)
+wwv_flow_api.create_plugin(
+ p_id=>wwv_flow_api.id(270922179615924351)
 ,p_plugin_type=>'REGION TYPE'
 ,p_name=>'OG_GOOGLE_MAPS_SEARCH'
 ,p_display_name=>'Buscador Google Maps'
+,p_supported_ui_types=>'DESKTOP'
 ,p_plsql_code=>wwv_flow_string.join(wwv_flow_t_varchar2(
 'function cargarMapa(',
 '    P_REGION IN APEX_PLUGIN.T_REGION, ',
@@ -71,19 +67,18 @@ wwv_flow_imp_shared.create_plugin(
 '    </div>'');',
 '    return null;',
 'end;'))
-,p_default_escape_mode=>'HTML'
 ,p_api_version=>2
 ,p_render_function=>'cargarMapa'
 ,p_standard_attributes=>'AJAX_ITEMS_TO_SUBMIT'
 ,p_substitute_attributes=>true
 ,p_subscribe_plugin_settings=>true
-,p_version_identifier=>'1.0'
+,p_version_identifier=>'1.5'
 );
 end;
 /
 prompt --application/end_environment
 begin
-wwv_flow_imp.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
 commit;
 end;
 /
